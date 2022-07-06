0200fd3b816a27b57542c5fd5df15393e0265aed
ShailJoshi <shailjoshi007@gmail.com>
2022-07-06 12:19:45 +0530

    Added a separate trining script

---
+++ src/models/triggertraining.py
@@ -0,0 +1,11 @@
+import numpy
+from CleanUpDf import *
+from FeatureArrays import *
+
+from RunProductionFn import *
+from TrainModel import *
+import pickle
+
+CleanUpDf()
+FeatureArrays()
+TrainModel()
